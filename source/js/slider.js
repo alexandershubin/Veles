39ecdc7fe5e734d3bee1071933dd219de2a634e1
alexandershubin <shubin8@mail.ru>
2020-05-04 02:06:42 +0300

    add slider-gallery and main block

--- source/js/slider.js
+++ source/js/slider.js
@@ -10,7 +10,33 @@ $('.slider__list--subheader').owlCarousel({
     768:{
       items: 1,
       nav: true,
+      dots: false,
+      navText: ['Предыдущая', 'Следующая']
+    }
+  }
+});
+
+$('.slider__list--gallery').owlCarousel({
+  loop:true,
+  margin:10,
+  responsive:{
+    0:{
+      items:1,
+      nav: false
+    },
+    768:{
+      margin:30,
+      items: 3,
+      nav: true,
+      dots: false,
+      center: true,
+      navText: ['&#9658;\t', '&#9658;\t']
+    },
+    1200:{
+      margin:30,
+      items: 4,
+      nav: true,
       dots: false
     }
   }
-})
+});
