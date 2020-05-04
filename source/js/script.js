a4683789d86600526ca804f7a8f898b682e1e92c
alexandershubin <shubin8@mail.ru>
2020-05-04 17:12:03 +0300

    fixed script on menu

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,10 @@
 'use strict'
 const buttonBurger = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__menu');
+const body = document.querySelector("body")
 
 buttonBurger.addEventListener('click', () => {
   buttonBurger.classList.toggle('active')
   headerMenu.classList.toggle('active')
+  body.classList.toggle('overflow')
 })
